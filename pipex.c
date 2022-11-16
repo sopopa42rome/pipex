70aea585d5a6f463a2e4ee3a13de0fa602f688c3
Sorin alexandru Popa <sopopa@e4r9p4.42roma.it>
2022-11-16 18:52:36 +0100

    Risolto un leak su find path piu varie

--- pipex.c
+++ pipex.c
@@ -6,18 +6,21 @@
 /*   By: sopopa <sopopa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 21:49:28 by sopopa            #+#    #+#             */
-/*   Updated: 2022/11/16 12:36:11 by sopopa           ###   ########.fr       */
+/*   Updated: 2022/11/16 17:50:50 by sopopa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+// fd[0] is for reading
+// fd[1] is for writing
 
 void    child_process(char **argv, char **envp, int *fd)
 {
 	int file_input;
-	
-	execute_command(argv[1], envp);
+	file_input = openfile(argv[1], O_READ);
+	dup2(fd[])
+	dup2(file_input, STDOUT_FILENO);
 
 }
 
--- pipex_utils.c
+++ pipex_utils.c
@@ -6,7 +6,7 @@
 /*   By: sopopa <sopopa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 21:51:17 by sopopa            #+#    #+#             */
-/*   Updated: 2022/11/16 14:51:38 by sopopa           ###   ########.fr       */
+/*   Updated: 2022/11/16 16:54:40 by sopopa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ void	error(void)
 }
 
 // Function that open the files with the right flags.
-int		openfile(char *argv, int mode)
+int	openfile(char *argv, int mode)
 {
-	int file;
+	int	file;
 
 	file = 0;
 	if (file == -1)
@@ -38,78 +38,42 @@ int		openfile(char *argv, int mode)
 
 void	execute_command(char *argv, char **envp)
 {
-	char **cmd;
+	char	**cmd;
+	int		i;
 
 	cmd = ft_split(argv, ' ');
 	if (execve(find_path(cmd[0], envp), cmd, envp) == -1)
 		error();
 }
 
-
-char	*find_path(char *argv_cmd, char **envp)
+char	*find_path(char *cmd, char **envp)
 {	
-	int i;
-	char	**split_of_envpath;
+	int		i;
+	char	**split_of_path;
 	char	*temp;
 	char	*path_with_cmd;
 
 	i = 0;
-	while (ft_strnstr(envp[i], "PATH", 4) != 0)
+	while (ft_strnstr(envp[i], "PATH=", 5) == NULL)
 		i++;
-	split_of_envpath = ft_split(envp[i] + 5, ':');
+	split_of_path = ft_split(envp[i] + 5, ':');
 	i = 0;
-	while (split_of_envpath[i] != NULL)
+	while (split_of_path[i] != NULL)
 	{
-		temp = ft_strjoin(split_of_envpath[i], "/");
-		free(split_of_envpath[i]);
-		path_with_cmd = ft_strjoin(temp, argv_cmd);
-		free(temp);	
+		temp = ft_strjoin(split_of_path[i], "/");
+		free(split_of_path[i]);
+		path_with_cmd = ft_strjoin(temp, cmd);
+		free(temp);
 		if (access(path_with_cmd, F_OK) == 0)
-		{
-			printf("%s", path_with_cmd);
 			return (path_with_cmd);
-		}
 		free(path_with_cmd);
-		i++;	
+		i++;
 	}
+	free(split_of_path);
 	return (0);
 }
 
-
-
-
-
-
-int main(int argc, char **argv, char **envp)
+int main (int argc, char **argv, char **envp)
 {
-	char *str;
-	
-	
-	str = find_path(argv[1], envp);
-	
+	execute_command(argv[1], envp);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-// {
-// 	if (argc == 2)
-// 	{
-// 		int bytes;
-// 		char *str;
-
-	
-// 		int file = openfile(argv[1], 2);
-// 		str = malloc(1000 * sizeof(str));
-// 		bytes = read(file, str, 1000);
-// 		printf("%s", str);
-// 	}
-// }
